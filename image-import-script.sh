3fdde9a5785a9e935b821276e6b464cc98a11cb1
m.salvi <m.salvi@reply.it>
2022-10-19 18:45:00 +0200

    data

--- image-import-script.sh
+++ image-import-script.sh
@@ -41,5 +41,5 @@ for image in "${found_images[@]}"; do
     new_reference="${TARGET_REGISTRY}/${image_path}"
   fi
   # Replace the image reference with the new reference in all the release-*.yaml
-  sed -E "s#image: ${image}#image: ${new_reference}#" output_${YAML_FOLDER}/release-*.yaml
+  sed -i.bak -E "s#image: ${image}#image: ${new_reference}#" ${YAML_FOLDER}/release-*.yaml
 done
